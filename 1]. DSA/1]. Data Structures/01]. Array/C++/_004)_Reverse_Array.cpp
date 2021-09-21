28408f3c48fb0d6f4b410d2f90c6c1d7f091459c
Shreyash Kale <78980467+shreyashkale9@users.noreply.github.com>
2021-09-21 21:46:56 +0530

    Update _004)_Reverse_Array.cpp
    Adding Comments

--- 1]. DSA/1]. Data Structures/01]. Array/C++/_004)_Reverse_Array.cpp
+++ 1]. DSA/1]. Data Structures/01]. Array/C++/_004)_Reverse_Array.cpp
@@ -2,11 +2,13 @@
 #include <cmath>
 using namespace std;
 
+//Function to reverse an array
  void reverse(int arr[], int n)
 {
 	int low = 0, high = n - 1;
 	while(low < high)
 	{
+		//Used temp variable to store the value temporary
 		int temp = arr[low];
 		arr[low] = arr[high];
 		arr[high] = temp;
@@ -20,13 +22,16 @@ int main()
 {
        int arr[] = {10, 5, 7, 30}, n = 4;
        cout<<"Before Reverse"<<endl;
+	// Print array before reversing
        for(int i = 0; i < n; i++)
        {
        		cout<<arr[i]<<" ";
        }
        cout<<endl;
+	// Fuction call
        reverse(arr, n);
        cout<<"After Reverse"<<endl;
+	// Print array after reversing
        for(int i = 0; i < n; i++)
        {
        		cout<<arr[i]<<" ";
